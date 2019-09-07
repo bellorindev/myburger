c1ab399af5b87dab6dfa35b47f8b91563c7b2410
José Bellorín <jlbellorin10@gmail.com>
2019-09-07 04:06:44 -0400

    Updates

---
+++ precache-manifest.2cb740c9a249ee81d3af7dce7a912a90.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "62d367f2e5ee27b447cabccb5beb9625",
+    "url": "/myburger/index.html"
+  },
+  {
+    "revision": "158416cd096cfbc3e6e9",
+    "url": "/myburger/static/css/3.ad48e111.chunk.css"
+  },
+  {
+    "revision": "adba0cea07c3f6c67efb",
+    "url": "/myburger/static/css/4.564d702f.chunk.css"
+  },
+  {
+    "revision": "57a825434b7cca16abf7",
+    "url": "/myburger/static/css/5.229bf5cd.chunk.css"
+  },
+  {
+    "revision": "29c9d41a8cf309e2e8fc",
+    "url": "/myburger/static/css/main.0083ecde.chunk.css"
+  },
+  {
+    "revision": "d8e22978595947688b05",
+    "url": "/myburger/static/js/2.39fbd0ef.chunk.js"
+  },
+  {
+    "revision": "158416cd096cfbc3e6e9",
+    "url": "/myburger/static/js/3.829c6b2a.chunk.js"
+  },
+  {
+    "revision": "adba0cea07c3f6c67efb",
+    "url": "/myburger/static/js/4.cba33898.chunk.js"
+  },
+  {
+    "revision": "57a825434b7cca16abf7",
+    "url": "/myburger/static/js/5.f602bb9b.chunk.js"
+  },
+  {
+    "revision": "29c9d41a8cf309e2e8fc",
+    "url": "/myburger/static/js/main.49847c7d.chunk.js"
+  },
+  {
+    "revision": "33c066be8e8cef326890",
+    "url": "/myburger/static/js/runtime~main.e726aa73.js"
+  },
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/myburger/static/media/burger-logo.b8503d26.png"
+  }
+]);
